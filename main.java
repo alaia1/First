0cecc801939f3d4b98a5230992c8368ca0005579
Alaia <trecualaia@gmail.com>
2024-06-09 21:22:25 +0200

    created a delete method

--- main.java
+++ main.java
@@ -3,4 +3,8 @@ public class main {
     public static void werty(String[] args) {
         System.out.println("fgh");
     }
+
+    public static void delete(){
+        String test="erty";
+    }
 }
